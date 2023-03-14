b8ec751f3199da00139a54955c450b6b4eab491f
Mateus Capoani Vieira <mateuscapoaniv@gmail.com>
2023-03-13 23:05:28 -0300

    adicao de container na pagina de not found

--- src/ui/screen/main/main.screen.jsx
+++ src/ui/screen/main/main.screen.jsx
@@ -1,4 +1,5 @@
 import { Container } from '../../component'
+
 export const MainScreen = () => {
 
   return (
--- src/ui/screen/not-found/not-found.screen.jsx
+++ src/ui/screen/not-found/not-found.screen.jsx
@@ -1,5 +1,12 @@
+import { Container } from '../../component'
+
 export const NotFoundScreen = () => {
+
   return (
-    <main>404 - Not Found</main>
+    <main>
+      <Container>
+        404 - Not Found
+      </Container>
+    </main>
   )
 }
